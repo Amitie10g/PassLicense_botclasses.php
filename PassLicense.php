3eaef644317acf85dd0fa0c98bd7b6786fd8e7ba
Davod <davod@localhost>
2015-12-01 16:42:40 -0300

    Committer: Davod 	new file:   PassLicense.php 	new file:   botclasses.php 	new file:   check_files.tpl.php

---
+++ PassLicense.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * PassLicense: botclases.php based MediaWiki for semiautomated license review
+ *
+ *  (c) 2015 Davod - https://commons.wikimedia.org/wiki/User:Amitie_10g
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ **/
+
+// Set the Error reporting if you need
+//error_reporting(E_ERROR | E_WARNING | E_PARSE /*| E_NOTICE */);
+error_reporting(E_ALL ^ E_WARNING & E_NOTICE);
+
+$user = 'Amitie 10g'; //DaBOT
+$password = 'FamiChan32???'; // 9c07957t1vbc7ec01fd1cdb7e723ec05c283598662c2a8d0c1
+$project = 'https://commons.wikimedia.org/w/api.php'; // http(s)://<wiki_url>/w/api.php
+
+$replace = '{{Indian navy}}';
+$with = '{{Indian navy|status=confirmed|reviewer=--~~~~}}';
+
+require_once('botclasses.php');
+
+$wiki = new wiki($project);
+
+define('IN_PassLicense',true);
+define('TEMP_PATH',realpath(sys_get_temp_dir()));
+
+if(isset($_GET['pass'])){
+
+	$login = $wiki->login($user,$password);
+	if($login['login']['result'] != 'Success') die('Not logged in. Check your user and password');
+
+	if(empty($_POST['pagename'])) die('No data given');
+	
+	$pages = $_POST['pagename'];
+
+	if(!empty($_POST['replace'])) $replace = $_POST['replace'];
+	if(!empty($_POST['with'])) $with = $_POST['with'];
+	
+	foreach($pages as $page){
+		$page = str_replace(' ','_',urldecode($page));
+		$content = $wiki->replacestring($page,$replace,$with);
+		$summary = 'License passed';
+		$result = $wiki->edit($page,$content,$summary);
+		var_dump($result);
+	}
+
+}else{
+	$category = $_GET['category'];
+	$categories = $wiki->categorymembers("Category:$category",50,false);
+	require_once('check_files.tpl.php');
+}
+?>
---
+++ check_files.tpl.php
@@ -0,0 +1,88 @@
+<?php if(!defined('IN_PassLicense')) die(); ?>
+<html>
+  <head>
+    <title>UploadFile (botclasses.php) by Davod</title>
+    <style>
+	body{
+		font-size:12pt;
+		font-family: Roboto, Arial sans-serif;
+	}
+	.checkbox, .thumb, .item, .thumb2, .col1, .col2{
+		vertical-align:middle;
+		display:table-cell;
+		border:5px #fff solid;
+	}
+	.element{
+		display:table-cell;
+		border-right:30px #fff solid;
+	}
+	.thumb{
+		width:70px;
+		text-align:center;
+	}
+	.element2{
+		margin:auto;
+		display:block;
+		border:2px #000 dotted;
+		width:600px;
+	}
+	.thumb2{
+		width:300px;
+	}
+	.upload_details{
+		padding:10px;
+		font-size:10pt
+	}
+	.collapse{
+		cursor:pointer;
+		-webkit-touch-callout: none;
+		-webkit-user-select: none;
+		-khtml-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+		float:right;
+	}	
+	.collapse + input{
+		display:none;
+	}
+	.collapse + input + *{
+		display:none;
+	}
+	.collapse+ input:checked + *{
+		display:block;
+	}
+	.details{
+		float:right;
+	}
+    </style>
+    
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+    <meta charset=utf-8 />
+   
+  </head>
+  <body>
+  
+  <h2>Select files</h2>
+<div>
+<form method="get" action="<?= $_SERVER['PHP_SELF'] ?>">
+<b>Enter a category:</b> <input style="width:300px" type="text" name="category" value="<?= $category ?>"><input type="submit">
+</form>
+<?php if(!empty($category)) { ?>
+<form method="post" action="<?= $_SERVER['PHP_SELF'] ?>?pass">
+<p>Replace <input type="text" name="replace"> with <input type="text" name="with"></p>
+<?php $num = 0; foreach($categories as $page){ $page = str_replace(' ','_',$page); if($num%2 == 0) $bg = 'DDD'; else $bg = 'EEE'; ?>
+<div style="background:#<?= $bg ?>;margin:auto">
+<input style="float:left !important" type="checkbox" name="pagename[]" value="<?= urlencode($page) ?>">
+<label style="float:left" class="collapse" for="<?= urlencode($page) ?>_details"><?= $page ?></label>
+<input id="<?= urlencode($page) ?>_details" type="checkbox">
+<div class="upload_details" id="<?= urlencode($page) ?>_details"> 
+<iframe style="width:100%;height:500px" src="https://commons.wikimedia.org/wiki/<?= urlencode($page) ?>?action=render"></iframe>
+</div>
+</div>
+<div style="clear:both">&nbsp;</div>
+<?php $num++; } ?>
+<p><input type="submit" value="Pass files"></p>
+</form>
+<?php } ?>
+</div>
