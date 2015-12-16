804ba7fc28b3e822107f5a7b24f547e26c34c8b4
Davod <davod@localhost>
2015-12-16 17:59:09 -0300

    Committer: Davod <davod@localhost> 	new file:   .htaccess 	new file:   doc/.htaccess 	modified:   doc/classes/PassLicense.html 	modified:   doc/classes/Wiki.html 	modified:   doc/classes/http.html 	modified:   doc/files/PassLicense.class.html 	modified:   doc/files/PassLicense.class.php.txt 	modified:   doc/graphs/class.html 	deleted:    doc/images/apple-touch-icon-114x114.png 	deleted:    doc/images/apple-touch-icon-72x72.png 	deleted:    doc/images/apple-touch-icon.png 	modified:   doc/images/favicon.ico 	modified:   doc/index.html 	deleted:    doc/lib/.htaccess 	deleted:    doc/lib/UploadFile.class.php 	deleted:    doc/lib/cli.php 	deleted:    doc/lib/web-add-info.tpl.php 	deleted:    doc/lib/web-body-intro.tpl.php 	deleted:    doc/lib/web-check-auth.tpl.php 	deleted:    doc/lib/web-footer.tpl.php 	deleted:    doc/lib/web-header.tpl.php 	deleted:    doc/lib/web-login-status.tpl.php 	deleted:    doc/lib/web-select-files.tpl.php 	deleted:    doc/lib/web-uploaded.tpl.php 	deleted:    doc/lib/web.php 	modified:   doc/namespaces/default.html 	modified:   doc/phpdoc-cache-b3/phpdoc-cache-file_402cc2c40e5f86ac460e64896af52518.dat 	modified:   doc/reports/deprecated.html 	modified:   doc/reports/errors.html 	modified:   doc/reports/markers.html 	modified:   index.html 	modified:   passed.html

--- doc/lib/web-check-auth.tpl.php
+++
@@ -1,2 +0,0 @@
-<?php if(!defined('IN_UploadFile')) die(); ?>
-<p><a href="<?= $_SERVER['PHP_SELF'] ?>?check_auth" target="_blank">Check for Authentication to Wiki</a></p>
--- doc/lib/web-login-status.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!defined('IN_UploadFile')) die(); ?>
-<p><b>Login status:</b> <?php if($login['login']['result'] == "Success") { ?>
-Successfully logged in as <b><?= $login['login']['lgusername'] ?></b>
-<?php }else{ ?>
-Error when login to your Wiki. Check your credentials in UploadFile-web.php
-<?php } ?></p>
